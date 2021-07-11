8e4df3126cb7e600061dad247bba1393f8d8f745
pcsaxena <poonamcsaxena@gmail.com>
2021-07-11 20:46:59 +0530

    added description

--- ETL_Program_1/Dealership.py
+++ ETL_Program_1/Dealership.py
@@ -1,3 +1,4 @@
+# ETL on used Cars Data
 import glob                         # this module helps in selecting files 
 import pandas as pd                 # this module helps in processing CSV files
 import xml.etree.ElementTree as ET  # this module helps in processing XML files.
